16ba8601ff600112d117e2f7e7c4faa932a82573
Gustavo Jitsuchaku <gustavohenrike@gmail.com>
2020-12-28 14:09:12 -0300

    wpp

---
+++ src/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "AIzaSyCokbypk-Q2P6KfWd7mDKRoFkNlHL6qsJk",
+    authDomain: "whatsappclone-75f8f.firebaseapp.com",
+    projectId: "whatsappclone-75f8f",
+    storageBucket: "whatsappclone-75f8f.appspot.com",
+    messagingSenderId: "780244493421",
+    appId: "1:780244493421:web:5f140d03c8fad9e8dc4b64"
+  };
